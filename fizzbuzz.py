9d820e420a13787e4438f3d001eb187b262505d1
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-02-26 14:41:01 -0500

    Updated from Brython Server: 2/26/2016 2:41:01 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -44,3 +44,21 @@ Fizz
 Fizz
 Buzz
 """
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
+
+myapp = App()
+myapp.run()
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
+bg = Sprite(bg_asset, (0,0))
+
+
+for x in range(100)
+    print
